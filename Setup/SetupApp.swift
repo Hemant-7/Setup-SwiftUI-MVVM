5a0034f59852cefa19022b22b1cd69c09b252428
Hemant Kumar <hmntkmr285@gmail.com>
2023-06-15 00:13:28 +0530

    Initial Commit

---
+++ Setup/SetupApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetupApp.swift
+//  Setup
+//
+//  Created by Hemant kumar on 15/06/23.
+//
+
+import SwiftUI
+
+@main
+struct SetupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SetupUITests/SetupUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SetupUITestsLaunchTests.swift
+//  SetupUITests
+//
+//  Created by Hemant kumar on 15/06/23.
+//
+
+import XCTest
+
+final class SetupUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
